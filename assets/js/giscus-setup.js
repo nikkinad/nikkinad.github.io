88a0dda8739d8bc33a30cd39b55ba732f2425d3a
nikkinad <156600327+nikkinad@users.noreply.github.com>
2026-03-12 15:19:06 +0000

    Deploying to gh-pages from @ nikkinad/nikkinad.github.io@f7edfff3eef1aefc000909676453e51dc9b23a20 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "nikkinad/nikkinadarevic.github.io",
+    "data-repo": "nikkinad/nikkinad.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
