7bb0cfcf98472d54ec1fc749233b841a6933be08
nikkinad <156600327+nikkinad@users.noreply.github.com>
2026-03-13 04:06:55 +0000

    Deploying to gh-pages from @ nikkinad/nikkinad.github.io@025b8ad7709b158c5adbe490dcf5963256324aa6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -518,28 +518,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("mailto:%6E%69%6B%6B%69.%6E%61%64%61%72%65%76%69%63@%64%61%6C.%63%61", "_blank");
         },
       },{
         id: 'social-custom_social',
